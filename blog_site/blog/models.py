ff4371062bc7728f62565318de3f99e148e132e9
SWilliams <Home@swilliamss-air.attlocal.net>
2018-08-07 18:59:16 -0700

    more corrections

--- blog_site/blog/models.py
+++ blog_site/blog/models.py
@@ -1,3 +1,5 @@
+# Define models Contributor, Editor, Post, Tag
+
 from django.db import models
 from django.urls import reverse
 
@@ -7,8 +9,9 @@ class Contributor(models.Model):
     uniqueId = models.CharField(max_length=20, help_text='ID')
     first_name = models.CharField(max_length=20, help_text='first name')
     last_name = models.CharField(max_length=20, help_text='last name')
-    #posts = 
-    #tags = 
+#    posts = models.ManyToManyField('Post') # use string for not-yet-defined parameter
+#    tags = models.ManyToManyField('Tag')  # use string for not-yet-defined parameter
+
     def get_absolute_url(self):
          """Returns the url to access a particular instance of MyModelName."""
          return reverse('model-detail-view', args=[str(self.id)])
@@ -17,12 +20,31 @@ class Contributor(models.Model):
      #   """String for representing the MyModelName object (in Admin site etc.)."""
       #  return self.first_name + ' ' + self.last_name 
 
+###########
+
+
+class Editor(models.Model):
+    uniqueId = models.CharField(max_length=20, help_text='ID')
+    first_name = models.CharField(max_length=20, help_text='first ame')
+    last_name = models.CharField(max_length=20, help_text='last name')
+#    posts = models.ManyToManyField('Post') # use string for not-yet-defined parameter
+#    tags = models.ManyToManyField('Tag')  # use string for not-yet-defined parameter
+    def get_absolute_url(self):
+         """Returns the url to access a particular instance of MyModelName."""
+         return reverse('model-detail-view', args=[str(self.id)])
+    
+    #def __str__(self):
+     #   """String for representing the MyModelName object (in Admin site etc.)."""
+      #  return self.first_name + ' ' + self.last_name 
+
+###########
+
 class Post(models.Model):
     uniqueId = models.CharField(max_length=20, help_text='ID')
     title = models.CharField(max_length=50, help_text='title')
     contributor = models.ManyToManyField(Contributor, help_text='Select contributors')  
     #design so that editors can add and choose from a list of contributors
-    #editor
+    editor = models.ManyToManyField(Editor)
     updated = models.DateField(auto_now=True)
     dek = models.CharField(max_length=50, help_text='summarize your post in one sentence')
     text = models.TextField(help_text='text')
@@ -41,23 +63,6 @@ class Post(models.Model):
     def __str__(self):
         """String for representing the MyModelName object (in Admin site etc.)."""
         return self.title
-
-
-###########
-
-
-class Editor(models.Model):
-    uniqueId = models.CharField(max_length=20, help_text='ID')
-    first_name = models.CharField(max_length=20, help_text='first ame')
-    last_name = models.CharField(max_length=20, help_text='last name')
-    posts = models.ManyToManyField(Post)
-    def get_absolute_url(self):
-         """Returns the url to access a particular instance of MyModelName."""
-         return reverse('model-detail-view', args=[str(self.id)])
-    
-    def __str__(self):
-        """String for representing the MyModelName object (in Admin site etc.)."""
-        return self.field_name
 
 
 ###########
